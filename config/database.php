cbf523e237174636b389fbde7b7aeb0c8e055d7b
islamshu12 <islamshu12@gmail.com>
2021-10-03 22:06:56 +0300

    mongo

--- config/database.php
+++ config/database.php
@@ -42,6 +42,17 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => [
+                 'database' =>  env('DB_DATABASE') // sets the authentication database required by mongo 3
+             ]
+         ],
 
         'mysql' => [
             'driver' => 'mysql',
